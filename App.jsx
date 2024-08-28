3bb9b10b77b6cd289e383e19f107c0bc5e6a34e7
se-bootcamp <nishaaby2725@gmail.com>
2024-08-27 23:06:05 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Modal from './Modal';
+import Footer from './Footer';
+import Header from './Header';
+import Hero from './Hero';
+import Modal from './Modal';
+
+
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <Hero />
+     <Footer />
+     <Modal />
+    </div>
+  );
+}
+
+export default App;
